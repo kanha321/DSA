73674c57ced81e7dd6eb10fb222dfe6e10b5391c
kanha321 <shubh.sg321@gmail.com>
2025-04-06 03:25:47 +0530

    03:25:47 AM

--- Math/isPrime.cpp
+++ Math/isPrime.cpp
@@ -4,15 +4,14 @@ using namespace std;
 bool isPrime(int x) {
     bool flag = true;
 
-    for (int i = 2; i < x; i++) {
-        if (x % i == 0) flag = false;
-    }
+    for (int i = 2; i < x; i++) if (x % i == 0) flag = false;
+
     return flag && x > 1;
 }
 
 int main() {
 
-    isPrime(0) ? cout<<"Yes" : cout<<"No";
+    isPrime(1) ? cout<<"Yes" : cout<<"No";
 
     return 0;
 }
